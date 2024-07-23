bf00cc3f3444fa09b777b16d61a413321af4babd
jasmin635v <jasmin_635v@hotmail.com>
2024-07-23 18:36:20 -0400

    fix

--- src/QAOA_jobs.py
+++ src/QAOA_jobs.py
@@ -433,7 +433,7 @@ def execute_slurmarray(all_jobs, task_id=None):
         execute_single_job(all_jobs[task_id])
 
 
-def job1_retrieve_merge_results(n_vertices, n_layers, n_steps, n_samples, n_isomorph_max, max_unlabeled_graph=None, max_job=None):
+def job1_retrieve_merge_results(n_vertices, n_layers, n_steps = None, n_samples = None, n_isomorph_max = None, max_unlabeled_graph=None, max_job=None):
 
     # graph, n_vertices, label
     result_names = get_possible_jobnames_from_params(
@@ -481,10 +481,8 @@ def job1_process_results(n_vertices, n_layers, n_steps, n_samples, n_isomorph_ma
 def job2_process_results(n_vertices, n_layers, n_steps= None, n_samples = None, n_isomorph_max = None, max_unlabeled_graph = None, max_job = None):
     print(f"start of result merge vertice {n_vertices}, layers {n_layers}, n_samples {n_samples}")
     # results_list = job1_retrieve_merge_results(n_vertices, n_layers, n_steps, n_samples, n_isomorph_max, max_unlabeled_graph, max_job)
-    results = job1_retrieve_merge_results(
-        n_vertices = n_vertices, n_layers = n_layers, n_samples = n_samples)
-    jobnames = get_job2_names_from_parameters(
-        n_vertices, n_layers, n_steps = n_steps, n_samples = n_samples, n_isomorph_max = n_isomorph_max, max_unlabeled_graph = max_unlabeled_graph, max_job = max_job)
+    results = job2_retrieve_merge_results(n_vertices = n_vertices, n_layers = n_layers, n_samples = n_samples)
+    jobnames = get_job2_names_from_parameters(n_vertices, n_layers, n_steps = n_steps, n_samples = n_samples, n_isomorph_max = n_isomorph_max, max_unlabeled_graph = max_unlabeled_graph, max_job = max_job)
     print("jobname obtained")
     process_results_save(results, jobnames)
 
